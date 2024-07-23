ac80be6b3c9ae7dedf152d42fa056e467c2c759c
Kevin <vidalmenares.k@gmail.com>
2024-07-23 19:07:59 -0400

    correccion detalles

--- ManekiApp/src/main/java/com/kevinvidal/controladores/ControladorNavegacion.java
+++ ManekiApp/src/main/java/com/kevinvidal/controladores/ControladorNavegacion.java
@@ -4,8 +4,6 @@ package com.kevinvidal.controladores;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
 import jakarta.servlet.http.HttpSession;
 
 
